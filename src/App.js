ed92aab0f63fc55c269068225c3a82ef6bbc469b
Rakesh Raj Mullick <jimmymorrios@gmail.com>
2025-07-27 18:22:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+// import Card from './components/Card'
 
 function App() {
+   const [data, setData] = useState("")
+   const [fData, setfData] = useState({
+    Temreture: "",
+    humidity: "",
+    Condition: "",
+    "Wind Speed" : ""
+   });
+   const [loading, setLoading] = useState(false);
+   const [isDataLoaded, setIsDataLoaded] = useState(false);
+   function handleChange(e){
+       
+      const input = e.target.value;
+      setData(input);
+   }
+
+   const fetchData = async ()=>{
+    setLoading(true)
+    setIsDataLoaded(false);
+    try{
+    
+    const res = await fetch (`https://api.weatherapi.com/v1/current.json?key=18618764b5e94caba4d80113241207&q=${data}`);
+    const fiData = await res.json();
+    
+    setfData({
+      Temreture : fiData.current.temp_c,
+      humidity : fiData.current.humidity,
+      Condition : fiData.current.condition.text,
+      "Wind Speed" : fiData.current.wind_mph
+    });
+     setIsDataLoaded(true);
+  }
+   catch(error){
+     alert("Failed to fetch weather data");
+   }
+    finally{
+      setLoading(false);
+    }
+   
+    
+    
+    // console.log(fData);
+    // console.log(await fData.current.humidity);
+    
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+        <input type={text} value={data} onChange={handleChange} placeholder='Enter city name' style={{height:30, width:180}}/>
+        <button onClick={fetchData} style={{color:'white', backgroundColor:'green', height:30, width:80, margin:10, borderRadius:5, cursor:'pointer'}}>Search</button>
+        </div>
+        {loading && <p>Loading data...</p>}
+        {!loading && isDataLoaded &&(
+        <div className='weather-cards'>
+           <div className='weather-card'>
+           {/* <strong>{condition}</strong> */}
+           <strong>Temprature</strong>
+           {fData.Temreture} C
+        </div>
+         <div className='weather-card'>
+           {/* <strong>{condition}</strong> */}
+           <strong>Humidity</strong>
+           {fData.humidity}%
+        </div>
+
+         <div className='weather-card'>
+           {/* <strong>{condition}</strong> */}
+           <strong>Codition</strong>
+           {fData.Condition}
+        </div>
+
+         <div className='weather-card'>
+           {/* <strong>{condition}</strong> */}
+           <strong>Wind Speed</strong>
+           {fData['Wind Speed']} kph
+        </div>
+       
+        </div>
+         )}
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// function Card() {
+//   return (
+//     <div>
+//         <Card style={{height:30, width:60, display:'flex', align: 'center', margin: 5, background:'white'}}>
+//            <strong>{condition}</strong>
+//            {}
+//         </Card>
+//     </div>
+//   )
+// }
+
+// export default Card
